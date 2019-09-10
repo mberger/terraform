2f90a20970743d7abb6b4387fe05d7a8b19a5f70
Michael Berger <mberger@internetinnovation.com>
2019-09-10 17:50:29 -0400

    a couple files

---
+++ connections.tf
@@ -0,0 +1,17 @@
+provider "google" {
+    credentials = "${file("../account.json")}"
+    project = "berger-lab-1"
+    region = "us-east1"
+}
+
+//provider "aws" {
+//    region = "us-east-1"
+//}
+
+//provider "azurerm" {
+//    subscription_id = "0"
+//    client_id = "1"
+//   client_secret = "2"
+//    tenant_id = "3"
+//
+//}
