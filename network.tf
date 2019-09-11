6004bc13e46f80b9e17132f91492c6931209d8c7
Michael Berger <mberger@internetinnovation.com>
2019-09-10 22:33:55 -0400

    another commit

--- network.tf
+++ network.tf
@@ -5,6 +5,14 @@ resource "google_compute_network" "our_dev_network" {
   
 }
 
+resource "google_compute_subnetwork" "dev-subnet" {
+    name = "dev-subnet"
+    ip_cidr_range = "10.0.1.0/24"
+    network = "${google_compute_network.our_dev_network.self_link}"
+    region = "us-east1"
+
+
+}
 //resource "aws_vpc" "env-example-two" {
  // cidr_block = "10.0.0.0/16"
  // enable_dns_hostnames = true
--- servers.tf
+++ servers.tf
@@ -1,20 +1,27 @@
 resource "google_compute_instance" "firstserver" {
     name = "thefirstserver"
-    machine_type = "n1-standart-1"
-    zone = "us-east-1a"  
+    machine_type = "n1-standard-1"
+    zone = "us-east1-b"  
+
+    tags = ["foo", "bar"]
 
     boot_disk {
         initialize_params { 
-            image = "debian-cloud/debian-8"
+            image = "debian-cloud/debian-9"
         }
     }
 
     network_interface {
-        subnetwork_project = "${google_compute_network.our_dev_network.name}"
+        subnetwork = "${google_compute_subnetwork.dev-subnet.name}"
+        
         access_config {
+            // ephemeral IP
         }
     }
 
+    metadata = {
+        foo = "bar"
+    }
     service_account {
         scopes = ["userinfo-email", "compute-ro", "storage-ro"]
         }
